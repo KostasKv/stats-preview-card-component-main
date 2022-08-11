b01d55f3b2d93be88c8a152cfece426d7df90c10
KostasKv <kostas.kvietinskas@gmail.com>
2022-08-11 17:17:04 +0100

    design implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {},
+    colors: {
+      'very-dark-blue-main-bg': 'hsl(233, 47%, 7%)',
+      'dark-desaturated-blue-card-bg': 'hsl(244, 38%, 16%)',
+      'soft-violet': 'hsl(277, 64%, 61%)',
+      'white': 'hsl(0, 0%, 100%)',
+      'slightly-transparent-white-main': 'hsla(0, 0%, 100%, 0.75)',
+      'slightly-transparent-white-stat': 'hsla(0, 0%, 100%, 0.6)',
+    },
+    fontFamily: {
+      'sans': ['Lexend Deca', 'sans-serif'],
+      'inter': ['Inter', 'sans-serif'],
+    },
+    screens: {
+      'sm': '1150px',
+    }
   },
   plugins: [],
 }
